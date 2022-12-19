642aa01b27ba356a62e0f125f7d7c87a493280b6
debrinaoct <debrinaoctrisya@gmail.com>
2022-12-20 01:07:13 +0700

    3rd commit

--- vendor.php
+++ vendor.php
@@ -141,7 +141,7 @@
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Vendor yang mengelola pembelian terbesar</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Vendor yang Menerima Pembelian Terbesar</h6>
                                     <div class="dropdown no-arrow">
                                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -169,7 +169,7 @@
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Persebaran Vendor di tiap Wilayah</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Persebaran Vendor Berdasarkan Ekspedisi Pengiriman</h6>
                                     
                                     <div class="dropdown no-arrow">
                                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
